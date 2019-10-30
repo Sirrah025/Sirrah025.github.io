13c29185ed10350c09ee2476ee89ac7553677f65
Will Harris <william.harris108@gmail.com>
2019-10-30 09:35:10 -0500

    Foundations for ajax

--- csci340/labs/lab7/stuff.js
+++ csci340/labs/lab7/stuff.js
@@ -1,8 +1,16 @@
 $(document).ready(function(){
   $("#clicker").click(function(){
-
+    $.ajax({
+      error: function(xhr,status,error) {
+        console.log(error);
+      }
+    });
   });
   $("#funnyClicker").click(function(){
-
+    $.ajax({
+      error: function(xhr,status,error) {
+        console.log(error);
+      }
+    });
   });
 });
