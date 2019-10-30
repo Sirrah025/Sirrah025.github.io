617ff156078be29cbcf2a62cd948d55254719aaa
Will Harris <william.harris108@gmail.com>
2019-10-30 13:37:02 -0500

    Adding Bored API

--- csci340/labs/lab7/stuff.js
+++ csci340/labs/lab7/stuff.js
@@ -1,6 +1,11 @@
 $(document).ready(function(){
   $("#clicker").click(function(){
     $.ajax({
+      datatype: "json",
+      url: "https://www.boredapi.com/api/activity/"
+      success: function(results){
+        $(".thing").text(results["activity"])
+      }
       error: function(xhr,status,error) {
         console.log(error);
       }
